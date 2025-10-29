a6323b6059770dfe4cec5b65391dda12bf13acf7
Lulozi <2319112614@qq.com>
2025-10-29 18:06:31 +0800

    优化 DoorWidgetPromptActivity 的 onCreate 方法，设置透明状态栏和导航栏颜色，并调整系统栏行为

--- android/app/src/main/kotlin/com/lulo/dormdevise/DoorWidgetPromptActivity.kt
+++ android/app/src/main/kotlin/com/lulo/dormdevise/DoorWidgetPromptActivity.kt
@@ -2,8 +2,11 @@ package com.lulo.dormdevise
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.RenderMode
 import io.flutter.embedding.android.TransparencyMode
@@ -57,6 +60,14 @@ class DoorWidgetPromptActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         window.setBackgroundDrawableResource(android.R.color.transparent)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        WindowCompat.getInsetsController(window, window.decorView)?.apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.statusBars())
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
         super.onCreate(savedInstanceState)
         markActive(true)
     }
