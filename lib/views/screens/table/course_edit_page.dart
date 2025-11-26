00e77c6b7fb253cdbba63b42eb584bc6c171cf4e
Lulozi <2319112614@qq.com>
2025-11-26 22:12:27 +0800

    优化课程编辑页面的预设颜色，更新为更柔和的色调以提升用户体验

--- lib/views/screens/table/course_edit_page.dart
+++ lib/views/screens/table/course_edit_page.dart
@@ -39,21 +39,22 @@ class _CourseEditPageState extends State<CourseEditPage> {
   int? _expandedSessionIndex;
 
   final List<Color> _presetColors = [
-    const Color(0xFFE57373),
-    const Color(0xFFF06292),
-    const Color(0xFFBA68C8),
-    const Color(0xFF9575CD),
-    const Color(0xFF7986CB),
-    const Color(0xFF64B5F6),
-    const Color(0xFF4FC3F7),
-    const Color(0xFF4DD0E1),
-    const Color(0xFF4DB6AC),
-    const Color(0xFF81C784),
-    const Color(0xFFAED581),
-    const Color(0xFFFFD54F),
-    const Color(0xFFFFB74D),
-    const Color(0xFFFF8A65),
-    const Color(0xFFA1887F),
+    const Color(0xFFFFCDD2), // Red 100
+    const Color(0xFFF8BBD0), // Pink 100
+    const Color(0xFFE1BEE7), // Purple 100
+    const Color(0xFFD1C4E9), // Deep Purple 100
+    const Color(0xFFC5CAE9), // Indigo 100
+    const Color(0xFFBBDEFB), // Blue 100
+    const Color(0xFFB3E5FC), // Light Blue 100
+    const Color(0xFFB2EBF2), // Cyan 100
+    const Color(0xFFB2DFDB), // Teal 100
+    const Color(0xFFC8E6C9), // Green 100
+    const Color(0xFFDCEDC8), // Light Green 100
+    const Color(0xFFF0F4C3), // Lime 100
+    const Color(0xFFFFF9C4), // Yellow 100
+    const Color(0xFFFFECB3), // Amber 100
+    const Color(0xFFFFE0B2), // Orange 100
+    const Color(0xFFFFCCBC), // Deep Orange 100
   ];
 
   @override
